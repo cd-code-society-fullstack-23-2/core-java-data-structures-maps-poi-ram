36c67af84d3f24ced884022919d530fb807384c0
Student 27 <student27@Codes-MacBook-Air-8.local>
2023-09-25 14:41:31 -0400

    added cities to MainPOI

--- src/main/java/com/codedifferentlty/labs/partD/MainPOIApp.java
+++ src/main/java/com/codedifferentlty/labs/partD/MainPOIApp.java
@@ -3,7 +3,8 @@ package com.codedifferentlty.labs.partD;
 public class MainPOIApp {
     public static void main(String[] args) {
         POI city1 = new POI("Wilmington", "1200 N French St", "A community educational building");
-
+        POI city2 = new POI("Wilmington", "111 W 10th St", "DECO Wilmington");
+        POI city3 = new POI("WWilmington", "608 N Market St", "Bardea Steak");
         System.out.print(city1.toString());
     }
 }
